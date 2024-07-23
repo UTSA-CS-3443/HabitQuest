8cfe48ddd9d0bb8e66b94177cbdbf124e019a625
Kat <katarah.griffin@my.utsa.edu>
2024-07-22 23:28:56 -0500

    Updated Each view to more closely align with our application design.

--- app/src/main/java/edu/usta/cs3443/habitquest/profile_Settings_Activity.java
+++ app/src/main/java/edu/usta/cs3443/habitquest/profile_Settings_Activity.java
@@ -14,7 +14,7 @@ public class profile_Settings_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_profile_settings);
+        setContentView(R.layout.activity_edit_profile_settings);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
